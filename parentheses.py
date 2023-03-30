44ca22a2b3c8445e6a10ae9483778f9163ddf772
Spaceiii <timothee01meyer@gmail.com>
2023-03-30 22:09:26 +0200

    suite

--- parentheses.py
+++ parentheses.py
@@ -1,5 +1,5 @@
 def valid_parentheses(expression: str) -> bool:
-    assert expression 
+    assert expression
     pile = []
     for char in expression:
         if char == "(":
@@ -30,16 +30,31 @@ def is_valid(expression: str) -> bool:
     return True
 
 
-def extract_data_from_parentheses(expression: str):
+def extract_data_from_parentheses(expression: str) -> list:
     parentheses_pos = parentheses_position(expression)
-    parentheses_pos.sort()
+    parentheses_pos.sort(reverse=True, key=lambda k: k[0])
     calculs = []
     while len(parentheses_pos) != 0:
-        pos1 = parentheses_pos.pop()[1]
-        pos2 = parentheses_pos.pop()[1]
-        print(pos1, pos2)
-        calculs.append(expression[pos1:pos2])
-    return calculs
+        pos1 = parentheses_pos.pop(0)[1]
+        pos2 = parentheses_pos.pop(0)[1]
+        print(pos1, pos2, expression[pos1+1:pos2])
+        calculs.append(expression[pos1+1:pos2])
+
+    return [iner_calcul(c) for c in calculs]
 
 
-print(extract_data_from_parentheses("(19)((18)())"))
+def iner_calcul(expression: str) -> float:
+    i = 0
+    for char in expression:
+        if char in ["+", "-", "/", "*"]:
+            break
+        i += 1
+    if i == len(expression):
+        return expression
+    symbol = expression[i]
+    if symbol == "+":
+        return int(expression[:i]) + int(expression[i+1:])
+    return int(expression[:i]) - int(expression[i+1:])
+
+
+print(extract_data_from_parentheses("(19)+((18)())"))
